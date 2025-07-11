bfe1c9c86161187abcc2515c2ebc02c27bc7bcc8
martincostello <martin@martincostello.com>
2025-07-11 10:46:04 +0100

    Add StabilizeVersion property
    Add new `StabilizeVersion` property which forces `VersionSuffix` to be blank when set to `true`.

---
+++ tests/BuildKit.Tests/VersionTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Martin Costello, 2025. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.BuildKit;
+
+public class VersionTests(ITestOutputHelper outputHelper)
+    : IntegrationTests(outputHelper)
+{
+    private static readonly string[] PropertyNames =
+    [
+        "VersionPrefix",
+        "VersionSuffix",
+    ];
+
+    [Fact]
+    public async Task StabilizeVersion_Does_Not_Set_VersionSuffix()
+    {
+        // Arrange
+        var properties = new Dictionary<string, string?>()
+        {
+            ["StabilizeVersion"] = "true",
+        };
+
+        // Act
+        var actual = await EvaluateProperties(
+            PropertyNames,
+            properties,
+            TestContext.Current.CancellationToken);
+
+        // Assert
+        actual.ShouldNotBeNull();
+        actual.ShouldContainKey("VersionPrefix");
+        actual.ShouldContainKeyAndValue("VersionSuffix", string.Empty);
+        actual["VersionPrefix"].ShouldNotBeNullOrWhiteSpace();
+    }
+}
