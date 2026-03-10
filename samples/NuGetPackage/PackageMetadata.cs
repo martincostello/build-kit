96601535a91da00e0b50259069362cc7b9d170d3
costellobot <102549341+costellobot@users.noreply.github.com>
2026-03-10 21:31:57 +0000

    Update .NET SDK to 10.0.200 (#380)
    * Update .NET SDK
    
    Update .NET SDK to version 10.0.200.
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.NET.Sdk
      dependency-version: 10.0.200
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: costellobot <102549341+costellobot@users.noreply.github.com>
    
    * Bump Microsoft.AspNetCore.Mvc.Testing from 10.0.3 to 10.0.4
    
    Bumps Microsoft.AspNetCore.Mvc.Testing from 10.0.3 to 10.0.4.
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.AspNetCore.Mvc.Testing
      dependency-version: 10.0.4
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: costellobot <102549341+costellobot@users.noreply.github.com>
    
    * Fix build
    
    Remove redundant suppression.
    
    * Fix build
    
    Fix nullability.
    
    * Fix build
    
    Update coverage threshold.
    
    ---------
    
    Signed-off-by: costellobot <102549341+costellobot@users.noreply.github.com>
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- samples/NuGetPackage/PackageMetadata.cs
+++ samples/NuGetPackage/PackageMetadata.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Martin Costello, 2025. All rights reserved.
+// Copyright (c) Martin Costello, 2025. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Reflection;
@@ -38,7 +38,7 @@ public static class PackageMetadata
     /// <summary>
     /// Gets the version of the package.
     /// </summary>
-    public static string Version { get; } = typeof(PackageMetadata).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+    public static string Version { get; } = typeof(PackageMetadata).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion!;
 
     private static string GetMetadataValue(string name, string defaultValue)
     {
